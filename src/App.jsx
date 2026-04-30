597bb117c9176716085bdc46ee52fe7e48b1fcd7
Emine Asanoska <ea32460@seeu.edu.mk>
2026-04-30 10:24:33 +0200

    Added derived data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, } from "react";
+import {useMemo} from "react";
 import UserCard from "./components/UserCard";
 import RegisterUser from "./components/RegisterUser";
 
--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,8 +1,6 @@
 
 
 
-
-
 import { useState } from "react";
 
 function RegisterUser({ addUser }) {
